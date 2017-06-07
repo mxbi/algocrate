2e270c6398a950a82bacec5a18b907e8c58de8f4
Mikel Bober-Irizar <mikel@mxbi.net>
2017-06-07 21:48:49 +0100

    Add python package

---
+++ algocrate/__init__.py
@@ -0,0 +1,5 @@
+from __future__ import print_function, division
+import numpy as np
+
+def test_print():
+	print('Algocrate works!')
---
+++ setup.py
@@ -0,0 +1,13 @@
+from distutils.core import setup
+
+setup(
+	name='algocrate',
+	packages=['algocrate'],
+	version='0.0.1',
+	author='Mikel Bober-Irizar',
+	author_email='mikel@mxbi.net',
+	url='https://github.com/mxbi/algocrate',
+	install_requires=[
+		'numpy'
+	]
+)
